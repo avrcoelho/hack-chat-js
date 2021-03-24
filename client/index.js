b3856ff97c3d569a52b42b0ff280035f2951ef03
avrcoelho <andrevrcoelho@hotmail.com>
2021-03-23 21:15:41 -0300

    feat: command cli class

--- client/index.js
+++ client/index.js
@@ -1,6 +1,17 @@
+/*
+  node index.js \
+  --username andrecoelho \
+  --room sala01 \
+  --hostUri localhost
+  
+ */
+
 import Events from "events";
+import CliConfig from "./src/cliConfig.js";
 import TerminalController from "./src/terminalController.js";
 
+const [nodePath, filePath, ...commands] = process.argv;
+const config = CliConfig.parseArguments(commands);
 const componentEmitter = new Events();
 
 const controller = new TerminalController();
